a4d28eba1d4967201f210912884644d5776f2cfa
ImGabreuw <imgabreuw.dev@gmail.com>
2021-08-11 16:19:06 -0300

    Primeiro commit

---
+++ src/test/java/me/gabreuw/springwebflux/SpringWebfluxApplicationTests.java
@@ -0,0 +1,13 @@
+package me.gabreuw.springwebflux;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWebfluxApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
