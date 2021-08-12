ec68b8db9908581132b1d393db67397bec9c1b18
ImGabreuw <imgabreuw.dev@gmail.com>
2021-08-12 16:13:07 -0300

    PLaylist Handler adicionado

---
+++ src/main/java/me/gabreuw/springwebflux/PlaylistHandler.java
@@ -0,0 +1,51 @@
+package me.gabreuw.springwebflux;
+
+import lombok.RequiredArgsConstructor;
+import me.gabreuw.springwebflux.document.Playlist;
+import me.gabreuw.springwebflux.service.PlaylistService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.server.ServerRequest;
+import org.springframework.web.reactive.function.server.ServerResponse;
+import reactor.core.publisher.Mono;
+
+import static org.springframework.http.MediaType.APPLICATION_JSON;
+import static org.springframework.web.reactive.function.BodyInserters.fromPublisher;
+
+@Component
+@RequiredArgsConstructor(onConstructor = @__(@Autowired))
+public class PlaylistHandler {
+
+    private final PlaylistService PLAYLIST_SERVICE;
+
+    public Mono<ServerResponse> findAll(ServerRequest request) {
+        return ServerResponse
+                .ok()
+                .contentType(APPLICATION_JSON)
+                .body(PLAYLIST_SERVICE.findAll(), Playlist.class);
+    }
+
+    public Mono<ServerResponse> findById(ServerRequest request) {
+        String id = request.pathVariable("id");
+
+        return ServerResponse
+                .ok()
+                .contentType(APPLICATION_JSON)
+                .body(PLAYLIST_SERVICE.findById(id), Playlist.class);
+    }
+
+    public Mono<ServerResponse> save(ServerRequest request) {
+        Mono<Playlist> playlist = request.bodyToMono(Playlist.class);
+
+        return ServerResponse
+                .created(request.uri())
+                .contentType(APPLICATION_JSON)
+                .body(
+                        fromPublisher(
+                                playlist.flatMap(PLAYLIST_SERVICE::save),
+                                Playlist.class
+                        )
+                );
+    }
+
+}
